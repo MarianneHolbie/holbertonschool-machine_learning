ea466a15ecd14972b87a96fe17d1679608143683
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2024-03-25 21:50:46 +0100

    restaure previous test with equalyto to 0 and 1 for P and Pr

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -74,10 +74,10 @@ def intersection(x, n, P, Pr):
         raise TypeError("P must be a 1D numpy.ndarray")
     if not isinstance(Pr, np.ndarray) or P.shape != Pr.shape:
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
-    if not np.all((P > 0) & (P < 1)) or np.all((Pr > 0) & (Pr < 1)):
-        if not np.all((P > 0) & (P < 1)):
+    if not np.all((P >= 0) & (P <= 1)) or np.all((Pr >= 0) & (Pr <= 1)):
+        if not np.all((P >= 0) & (P <= 1)):
             raise ValueError("All values in P must be in the range [0, 1]")
-        if not np.all((Pr > 0) & (Pr < 1)):
+        if not np.all((Pr >= 0) & (Pr <= 1)):
             raise ValueError("All values in Pr must be in the range [0, 1]")
 
     if np.sum(Pr) != 1:
