21a0701a4b77d6cf61d5f61c477d59bcbe2de36e
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2024-03-25 14:39:24 +0100

    solve pep 8 + change test case on x, because x can be egal to 0 + test len(P.shape)

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -13,7 +13,8 @@ def likelihood(x, n, P):
 
     :param x: number of patients that develop severe side effects
     :param n: total number of patients observed
-    :param P: 1D ndarray, various hypothetical proba of developing severe side effects
+    :param P: 1D ndarray, various hypothetical proba of developing
+     severe side effects
 
     :return: 1D ndarray, containing likelihood of obtaining data, x and n
         for each probability in P, respectively
@@ -26,12 +27,13 @@ def likelihood(x, n, P):
 
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
-    if not isinstance(x, int) or x <= 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+    if not isinstance(x, int) or x < 0:
+        raise (ValueError
+               ("x must be an integer that is greater than or equal to 0"))
 
     if x > n:
         raise ValueError("x cannot be greater than n")
-    if not isinstance(P, np.ndarray):
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
     if not np.all((P >= 0) & (P <= 1)):
         raise ValueError("All values in P must be in the range [0, 1]")
