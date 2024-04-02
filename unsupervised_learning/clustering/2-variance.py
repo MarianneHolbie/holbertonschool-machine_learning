695487cfc2aa2c39ea3643f8715dcc98164549e9
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2024-04-02 11:30:37 +0200

    add test case for X and C

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -16,6 +16,12 @@ def variance(X, C):
         var: total variance
     """
 
+    if X.ndim != 2 or C.ndim != 2:
+        return None
+
+    if X.shape[1] != C.shape[1]:
+        return None
+
     distance = np.linalg.norm(X[:, np.newaxis] - C, axis=2)
     min_dist = np.min(distance, axis=1)
     var = np.sum(min_dist ** 2)
