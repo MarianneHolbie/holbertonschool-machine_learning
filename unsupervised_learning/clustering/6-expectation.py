4793ff537cb1869d771b0db5aa9ff6277574b291
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2024-04-03 10:00:51 +0200

    modify test on shape pi

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -22,7 +22,7 @@ def expectation(X, pi, m, S):
     """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None
-    if not isinstance(pi, np.ndarray) or len(pi.shape) != 1:
+    if not isinstance(pi, np.ndarray) or len(pi.shape) != 2:
         return None, None
     if not isinstance(m, np.ndarray) or len(m.shape) != 2:
         return None, None
