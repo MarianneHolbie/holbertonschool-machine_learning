362f3c643ed0c3b7c3e6e3f948b967d603feac12
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2024-05-14 10:13:40 +0200

    generate all n-gram

--- supervised_learning/nlp_metrics/1-ngram_bleu.py
+++ supervised_learning/nlp_metrics/1-ngram_bleu.py
@@ -7,54 +7,53 @@ from collections import Counter
 import numpy as np
 
 
-def generate_ngram(sentence, n):
+def generate_ngram(sentence, order):
     """
-        split list of sentences in n-gram
+        genrate n-grams for all orders from 1 to max_order
 
     :param sentence: sentence to split
-    :param n: number of n-gram
+    :param order: number of n-gram
 
     :return: list of formed n-gram
     """
-    if n <= 1:
-        return sentence
+    ngrams = []
+    for i in range(len(sentence) - order + 1):
+        ngram = sentence[i: i + order]
+        ngrams.append(' '.join(ngram))
 
-    step = n - 1
-    result = sentence[:-step]
-
-    for i in range(len(result)):
-        for j in range(step):
-            result[i] += ' ' + sentence[i + 1 + j]
-
-    return result
+    return ngrams
 
 
-def modified_precision(references, sentence, max_order):
+def modified_precision(references, sentence, order):
     """
         calculate modified ngram precision
 
     :param references: list of reference translations
     :param sentence: list containing the model proposed sentence
-    :param max_order: size of the n-gram to use for evaluation
+    :param order: size of the n-gram to use for evaluation
 
     :return: modified precision
     """
-    counts = Counter(generate_ngram(sentence, max_order)) if len(sentence) >= max_order else Counter()
+    sentence_ngrams = Counter(generate_ngram(sentence, order))
+
+    if not sentence_ngrams:
+        return 0
 
     max_counts = {}
     for reference in references:
-        ref_counts = (Counter(generate_ngram(reference, max_order)) if len(reference) >= max_order else Counter())
-
-        for ngram in counts:
-            max_counts[ngram] = max(max_counts.get(ngram, 0), ref_counts[ngram])
+        ref_ngrams = Counter(generate_ngram(reference, order))
+        for ngram in sentence_ngrams:
+            max_counts[ngram] = max(max_counts.get(ngram, 0),
+                                    ref_ngrams[ngram])
 
     # intersection between hypothesis and reference's count
     clipped_counts = {
-        ngram: min(count, max_counts[ngram]) for ngram, count in counts.items()
+        ngram: min(count, max_counts.get(ngram, 0))
+        for ngram, count in sentence_ngrams.items()
     }
 
     numerator = sum(clipped_counts.values())
-    denominator = max(1, len(sentence))
+    denominator = max(1, len(sentence) - order + 1)
 
     return numerator / denominator
 
@@ -71,21 +70,17 @@ def ngram_bleu(references, sentence, max_order):
     """
 
     len_sentence = len(sentence)
-    ngram_precisions = []
-
-    for order in range(1, max_order + 1):
-        modified_precisions = modified_precision(references, sentence, order)
-        ngram_precisions.append(modified_precisions)
-
-    # len reference
-    closest_ref_len = min((abs(len(ref) - len_sentence), len(ref)) for ref in references)[1]
+    ngram_precisions = [modified_precision(references, sentence, order)
+                        for order in range(1, max_order + 1)]
 
     # BP
-    if len_sentence > closest_ref_len:
+    min_ref_length = min(len(ref) for ref in references)
+    if len_sentence >= min_ref_length:
         BP = 1
     else:
-        BP = np.exp(1 - closest_ref_len / len_sentence)
+        BP = np.exp(1 - min_ref_length / len_sentence)
 
-    bleu_score = BP * np.exp(sum(np.log(p) for p in ngram_precisions) / max_order)
+    bleu_score = BP * np.exp(sum(np.log(p) for p in ngram_precisions)
+                             / max_order)
 
     return bleu_score
