6e7aaeadf323256d7cacbd613ab19db6a06693c1
MarianneHolbie <112870394+MarianneHolbie@users.noreply.github.com>
2024-07-15 10:24:13 +0200

    add function to compute Monte-Carlo gradient policy

--- reinforcement_learning/policy_gradients/policy_gradient.py
+++ reinforcement_learning/policy_gradients/policy_gradient.py
@@ -23,3 +23,32 @@ def policy(matrix, weight):
     softmax = exp_z / np.sum(exp_z, axis=1, keepdims=True)
 
     return softmax
+
+
+def policy_gradient(state, weight):
+    """
+        function that computes the Monte-Carlo policy gradient
+
+    :param state: matrix, current observation of the env
+    :param weight: matrix of random weight
+
+    :return: action and gradient
+    """
+    # calculate policy
+    probs = policy(state, weight)
+
+    # choose action based on probs
+    action = np.random.choice(probs.shape[1], p=probs[0])
+
+    # init gradients
+    grad = np.zeros_like(weight)
+
+    # calculate gradients
+    for i in range(state.shape[1]):
+        for j in range(weight.shape[1]):
+            if j == action:
+                grad[i, j] = state[0, i] * (1 - probs[0, j])
+            else:
+                grad[i, j] = -state[0, i] * probs[0, j]
+
+    return action, grad
