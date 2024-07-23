c85a275d07e44fb2c46acd3046b37b892b9694c4
MarianneHolbie <112870394+MarianneHolbie@users.noreply.github.com>
2024-07-23 21:57:12 +0200

    list all shows that have at least one genre linked

---
+++ pipeline/databases/8-genre_id_by_show.sql
@@ -0,0 +1,4 @@
+-- lists all shows that have one genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
