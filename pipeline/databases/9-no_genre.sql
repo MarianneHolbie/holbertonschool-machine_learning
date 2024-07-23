9f5545c5ef0d06a697def553142acaa555ea6653
MarianneHolbie <112870394+MarianneHolbie@users.noreply.github.com>
2024-07-23 22:14:34 +0200

    change = in IS + left join

--- pipeline/databases/9-no_genre.sql
+++ pipeline/databases/9-no_genre.sql
@@ -1,7 +1,7 @@
 -- No genre
 SELECT ts.title, tsg.genre_id
 FROM tv_shows AS ts
-JOIN tv_show_genres AS tsg
+LEFT JOIN tv_show_genres AS tsg
 ON ts.id = tsg.show_id
-WHERE tsg.genre_id = NULL
+WHERE tsg.genre_id IS NULL
 ORDER BY ts.title ASC, tsg.genre_id ASC;
