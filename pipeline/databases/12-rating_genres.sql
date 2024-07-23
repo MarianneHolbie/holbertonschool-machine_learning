38f81e31191543b85ce1e31e0a2775765278e317
MarianneHolbie <112870394+MarianneHolbie@users.noreply.github.com>
2024-07-23 22:36:55 +0200

    solve first join

--- pipeline/databases/12-rating_genres.sql
+++ pipeline/databases/12-rating_genres.sql
@@ -2,8 +2,7 @@
 SELECT tg.name, SUM(tsr.rate) AS rating
 FROM tv_genre AS tg
 JOIN tv_show_genres AS tsg
-ON tsr.show_id = ts.id
-GROUP BY tsg.genre_id = tg.id
+ON tsg.genre_id = tg.id
 JOIN tsr
 ON tsr.show_id = tsg.show_id
 GROUP BY tg.name
