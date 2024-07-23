0cd57e03d0b14f1a38a75b7f975740c46105986f
MarianneHolbie <112870394+MarianneHolbie@users.noreply.github.com>
2024-07-23 10:21:28 +0200

    print location of a specific GitHub user

---
+++ pipeline/apis/2-user_location.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Rate me is you can!
+"""
+import requests
+import sys
+import time
+
+
+if __name__ == '__main__':
+    # invalid command
+    if len(sys.argv) != 2:
+        exit()
+
+    url = sys.argv[1]
+    # default headers
+    headers = {'Accept': 'application/vnd.github.v3+json'}
+
+    results = requests.get(url, headers=headers)
+
+    if results.status_code == 200:
+        print(results.json()["location"])
+
+    elif results.status_code == 404:
+        print("Not found")
+
+    elif results.status_code == 403:
+        rate_lim = int(results.headers['X-Ratelimit-Reset'])
+        now = int(time.time())
+        minutes = int((rate_lim - now) / 60)
+        print("Reset in {} min".format(minutes))
