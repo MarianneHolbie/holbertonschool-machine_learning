ab376eeb420ea2fcafd8eb49a8ed97d37e7881fd
MarianneHolbie <112870394+MarianneHolbie@users.noreply.github.com>
2024-07-23 22:08:44 +0200

    correction name table

--- pipeline/databases/8-genre_id_by_show.sql
+++ pipeline/databases/8-genre_id_by_show.sql
@@ -1,6 +1,6 @@
--- lists all shows that have one genre linked
+-- lists that have one genre linked
 SELECT ts.title, tsg.genre_id
-FROM tv_show AS ts
+FROM tv_shows AS ts
 JOIN tv_show_genres AS tsg
 ON ts.id = tsg.show_id
 ORDER BY ts.title ASC, tsg.genre_id;
