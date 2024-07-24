3843a7361ba775fdb72184b8ab1ac8111fa42792
MarianneHolbie <112870394+MarianneHolbie@users.noreply.github.com>
2024-07-24 17:05:47 +0200

    create function to count

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -5,23 +5,28 @@
 from pymongo import MongoClient
 
 
+def count_function(collection, method):
+    """
+        Method to count from nginx log
+
+    :param collection: name of collection
+    :param method: selected method
+
+    :return: count
+    """
+    return collection.count_documents({"method": method})
+
+
 if __name__ == "__main__":
     # connect MongoDB and load db and collection
     client = MongoClient('mongodb://127.0.0.1:27017')
     logs = client.logs.nginx
 
-    # number of doc in collection
-    nb_doc = logs.count_documents({})
-    print("{} logs".format(nb_doc))
-
-    # number doc with different methods
-    print("Methods:")
     methods_name = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for m in methods_name:
-        count = logs.count_documents({"method": m})
-        print("\tmethod {}: {}".format(m, count))
 
-    # number doc with get and status
-    filter_doc = {"method": "GET", "path": "/status"}
-    path_count = logs.count_documents(filter_doc)
-    print("{} status check".format(path_count))
+    print("{} logs".format(logs.count_function({})))
+    print("Methods:")
+    for method in methods_name:
+        print("\tmethod {}: {}".format(method, count_function(logs, method)))
+    print("{} status check".format(logs.count_documents({"method": "GET",
+                                                         "path": "/status"})))
