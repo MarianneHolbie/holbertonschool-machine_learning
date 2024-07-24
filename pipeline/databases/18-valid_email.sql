1d162b75287c0645d96ad4d73fb5f327aa212e5c
MarianneHolbie <112870394+MarianneHolbie@users.noreply.github.com>
2024-07-24 09:39:35 +0200

    trigger to update change email

---
+++ pipeline/databases/18-valid_email.sql
@@ -0,0 +1,6 @@
+-- trigger resets attribute valid_email if it change
+CREATE TRIGGER UpdateMail
+BEFORE UPDATE ON users
+FOR EACH ROW
+    IF NEW.email != OLD.email THEN
+    SET NEW.valid_email = 0;
