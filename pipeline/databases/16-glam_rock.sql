894667b9eb8b1340455c23c624a257d33d8eede8
MarianneHolbie <112870394+MarianneHolbie@users.noreply.github.com>
2024-07-24 09:05:50 +0200

    change if condition

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -1,5 +1,7 @@
 -- old school band
-SELECT band_name,  IF NULL(split, 2020) - formed AS lifespan
+SELECT band_name,  IF (
+  split IS NULL, (2020 - formed), (split - formed)
+) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
