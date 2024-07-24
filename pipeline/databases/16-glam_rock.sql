44b68aeab401d208b3cc04fe259978719475fc18
MarianneHolbie <112870394+MarianneHolbie@users.noreply.github.com>
2024-07-24 09:04:16 +0200

    suppress YEAR

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -1,5 +1,5 @@
 -- old school band
-SELECT band_name, IF (split is NULL, YEAR(2020, split) - formed AS lifespan
+SELECT band_name,  IF NULL(split, 2020) - formed AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
