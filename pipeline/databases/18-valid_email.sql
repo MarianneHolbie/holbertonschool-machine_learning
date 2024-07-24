3bea99ed4aefe9928389e019182ea8a0df495b79
MarianneHolbie <112870394+MarianneHolbie@users.noreply.github.com>
2024-07-24 09:43:25 +0200

    add end if and end

--- pipeline/databases/18-valid_email.sql
+++ pipeline/databases/18-valid_email.sql
@@ -3,4 +3,6 @@ CREATE TRIGGER UpdateMail
 BEFORE UPDATE ON users
 FOR EACH ROW
     IF NEW.email != OLD.email THEN
-    SET NEW.valid_email = 0;
+        SET NEW.valid_email = 0;
+        END IF;
+END;
